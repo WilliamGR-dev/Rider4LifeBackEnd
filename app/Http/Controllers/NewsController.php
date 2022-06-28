f9da293d1a647c3a967f8aad5826ba2b3a07521b
WilliamGR-dev <wgirardreydet@gmail.com>
2022-06-28 09:50:41 +0200

    pass count publication in response Get News by user id

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -56,6 +56,7 @@ class NewsController extends Controller
     public function getNewsByUserId($id)
     {
         $news = DB::table('news')->where('user_id', $id)->get();
+        $publications = DB::table('news')->where('user_id', $id)->count();
 
         $author = DB::table('users')->where('id', $id)->first();
 
@@ -65,6 +66,7 @@ class NewsController extends Controller
         $data->results = $news;
         $data->author = $author;
         $data->likes = $likes;
+        $data->publications = $publications;
 
         return response()->json($data, 200);
     }
